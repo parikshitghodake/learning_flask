523a23368a8cc1d99ee3e28a025c24b815990ff3
Parikshit <parikshitghodake@gmail.com>
2017-02-14 23:41:22 +0530

    latest version

---
+++ forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import Form
+from wtforms import StringField , PasswordField, SubmitField
+from wtforms.validators import DataRequired , Email , Length
+
+class SignupForm(Form) :
+	first_name = StringField('First Name' , validators=[DataRequired('Please Enter your First Name')])
+	last_name = StringField('Last Name', validators=[DataRequired('Please Enter your Last Name')])
+	email = StringField('Email', validators=[DataRequired('Please Enter your Email address') , Email('Please Enter Valid Email Address')])
+	password = PasswordField('Password', validators=[DataRequired('Please Enter your Password') , Length(min=6 , message='Password Should be min of 6 characters')])
+	submit = SubmitField('Sign Up')
+
+class LoginForm(Form):
+	email = StringField('Email', validators=[DataRequired('Please Enter your Email Address') , Email('Please Enter Valid Email Address')])
+	password = PasswordField('Password' , validators=[DataRequired('Please Enter your Password') , Length(min=6 , message='Password Should be min of 6 characters')])
+	submit = SubmitField('Sign In')
+
+class FeedbackForm(Form):
+	first_name = StringField('First Name' , validators=[DataRequired('Please Enter your Name')])
+	email = StringField('Email', validators=[DataRequired('Please Enter Your Email Address') , Email('Please Enter Valid Email')])
+	comments = StringField('Comments' , validators=[DataRequired('Please enter your comments')])
+	submit = SubmitField('Submit')
+
+class SearchMovie(Form):
+	movie_name = StringField('Movie Name' , validators=[DataRequired('Enter Movie Name')])
+	submit = SubmitField('Search')
--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
 
+import urllib2
+import json
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -22,3 +25,49 @@ class User(db.Model):
 
   def check_password(self, password):
     return check_password_hash(self.pwdhash, password)
+
+
+class Feedback(db.Model):
+  __tablename__ = 'feedback'
+  uid = db.Column(db.Integer, primary_key = True)
+  firstname = db.Column(db.String(100))
+  email = db.Column(db.String(120))
+  comments = db.Column(db.String(500))
+
+  def __init__(self, firstname, email, comments):
+    self.firstname = firstname.title()
+    self.email = email.lower()
+    self.comments = comments.title()
+
+
+class Movie(object):
+  def query(self,moviename) :
+    query_url = 'http://netflixroulette.net/api/api.php?title=' + moviename
+    m = urllib2.urlopen(query_url)
+    movielist = m.read()
+    m.close()
+
+    data = json.loads(movielist)
+    
+    """title = data['show_title']
+    category = data['category']
+    year = data['release_year']
+    poster = data['poster']
+    cast = data['show_cast']
+    summary = data['summary']
+    director = data['director']
+    runtime = data['runtime']
+
+    movieobj = {
+    'title' : title,
+    'category' : category,
+    'year' : year,
+    'poster':poster,
+    'director':director,
+    'summary':summary,
+    'cast':cast
+    }"""
+
+
+
+    return data
